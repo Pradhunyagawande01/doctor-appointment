2661769fa2c30e121b8cedbcf8746f6a07c028ff
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2024-08-22 21:02:04 +0530

    Components updated

--- npx/src/layout/layout.jsx
+++ npx/src/layout/layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Routers from 'react-router-dom';
 
 const layout = () => {
   return (
     <div>
-      
+        <Header />
+            <main><Routers /></main>   
+        <Footer />  
     </div>
   )
 }
--- npx/src/routes/Routers.jsx
+++ npx/src/routes/Routers.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react' 
+import Homee from '../pages/Home';
+import Services from '../pages/Services';
+import Login from '../pages/Login'; 
+import Signup from '../pages/Signup';
+import Contact from '../pages/Contact';
+import Doctors from '../pages/doctors/Doctors';
+import DoctorsDetails from '../pages/doctors/DoctorsDetails';
+import { Routes, Route  } from 'react-router-dom';
 
 const Routers = () => {
   return (
     <div>
+        <Routes>
+            <Route path="/" element={<Homee />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/doctors" element={<Doctors />} />
+            <Route path="/doctors/:id" element={<DoctorsDetails />} />
+        </Routes>
       
     </div>
   )
